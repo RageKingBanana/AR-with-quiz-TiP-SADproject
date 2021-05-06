a7fc92eae062bf08b2eccf159bf31647cc26bc73
NoobMaster <bk201_perez@yahoo.com>
2021-05-06 17:57:59 +0800

    renamed newbehaviour script to RESETLEVELS

--- Assets/Scripts/lr_Linecontroller.cs
+++ Assets/Scripts/lr_Linecontroller.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public LevelSelection levelselection;
-    public void reset()
+    void Start()
     {
-        PlayerPrefs.SetString("levelU", "10000");
-        levelselection.NiceFunc();
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
